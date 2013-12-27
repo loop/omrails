a926a25625765f22dc2bf541603ab0064fd7cc7c
Yogesh Nagarur <yogeshnagarur@gmail.com>
2013-12-27 00:49:00 +0000

    Added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Omrails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => 'http://dry-brushlands-9754.herokuapp.com' }
 end
